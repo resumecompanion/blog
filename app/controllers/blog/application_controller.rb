061f3237a237a25199a9d6d1a0efe25e1bc33e47
hellolucky <hellolucky123@gmail.com>
2012-11-30 15:35:04 +0800

    add recent posts

--- app/controllers/blog/application_controller.rb
+++ app/controllers/blog/application_controller.rb
@@ -1,7 +1,14 @@
 module Blog
   class ApplicationController < ActionController::Base
+
+    before_filter :find_recent_posts
+
     def require_admin
       redirect_to blog.root_path unless blog_user_signed_in? && current_blog_user.is_admin?
+    end
+
+    def find_recent_posts
+      @recent_posts = Blog::Post.where("is_published = true and published_at < '#{Time.now()}'").order("published_at DESC").limit(10)
     end
   end
 end
