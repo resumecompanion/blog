fca654426e2cd67681ef696979bfd8e0a2c9d653
hellolucky <hellolucky123@gmail.com>
2013-01-08 13:58:38 +0800

    add filename field for image

--- app/models/blog/file.rb
+++ app/models/blog/file.rb
@@ -1,6 +1,6 @@
 module Blog
   class File < ActiveRecord::Base
-    attr_accessible :image, :title
+    attr_accessible :image, :title, :filename
 
     mount_uploader :image, Blog::ImageUploader
     paginates_per 48
@@ -8,5 +8,13 @@ module Blog
     validates_presence_of :image
 
     belongs_to :user, :foreign_key => :blog_user_id
+
+    def filename=(filename)
+      self.image.rename(filename) if filename.present?
+    end
+
+    def filename
+      ::File.basename(self.image.path).split(".").first
+    end
   end
 end
