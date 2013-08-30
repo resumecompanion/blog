d2a0e58bec200f86e038f396354d7277c250686a
Josh Moore <joshsmoore@gmail.com>
2013-08-30 15:40:52 +0800

    move CMS assets into CMS engine

--- lib/blog/engine.rb
+++ lib/blog/engine.rb
@@ -5,5 +5,11 @@ module Blog
     config.app_middleware.insert_before(Rack::Lock, Rack::Rewrite) do
       r301 %r{^/(.*)/$}, '/$1'
     end
+
+    initializer "cms.precompile_assets" do |app|
+      app.config.assets.precompile += %w( blog/application.js, blog/application.css blog/admin.js blog/admin.css )
+      app.config.assets.precompile += %w( blog/plugin/ckeditor/init.js blog/plugin/ckeditor/*)
+    end
+    
   end
 end
